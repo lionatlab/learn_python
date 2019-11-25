0fa790d8c679016e48439e6ec1dafb9a2585e4d4
mschu <chuminsung@gmail.com>
2019-11-25 23:00:54 +0900

    camera_mnist - ikess 버전 이용

--- mschu/README_mschu.md
+++ mschu/README_mschu.md
@@ -6,7 +6,7 @@
   - 에러 : Process finished with exit code 134 (interrupted by signal 6: SIGABRT).  
   - 해결 : 해당문제는 파이참에서의 문제이므로, 터미널에서 실행.  
 3. 숫자 비추면 무슨 숫자인지 알아맞추기.  
-  3-1. numpy 어레이로 바꾸기.  
+  3-1. 카메라 이미지 gray 로 변경 / training image와 크기 맞추기  
   3-2. tensorflow mnist 튜토리얼 돌려본다.  
   3-3. 정확도 향상시키기
 5. 각 폴더 이름만들고 frontend 폴더 안에 소스 저장.  
--- mschu/frontend/camera_mnist.py
+++ mschu/frontend/camera_mnist.py
@@ -1,117 +1,68 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-import os
-import cv2
-
-# TensorFlow and tf.keras
 import tensorflow as tf
-from tensorflow import keras
-
-# Helper libraries
 import numpy as np
-import matplotlib.pyplot as plt
-import random
-
-# handling warning
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # pair with import os
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'  # omp error #15 initializing libiomp5.dylib but found libiomp5.dylib
-# already initialized
-print(tf.__version__)
-
-# video capture
-capture = cv2.VideoCapture(0)
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
-# Keras provides a handy API to download the MNIST dataset, and split them into
-# "train" dataset and "test" dataset.
-mnist = keras.datasets.mnist
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-
-# Normalize the input image so that each pixel value is between 0 to 1.
-train_images = train_images / 255.0
-test_images = test_images / 255.0
-print('Pixels are normalized')
-
-# Show the first 25 images in the training dataset.
-plt.figure(figsize=(10, 10))
-for i in range(25):
-    plt.subplot(5, 5, i + 1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.gray)
-    plt.xlabel(train_labels[i])
-# plt.show()
-
-# Define the model architecture
-model = keras.Sequential([
-    keras.layers.InputLayer(input_shape=(28, 28)),
-    keras.layers.Reshape(target_shape=(28, 28, 1)),
-    keras.layers.Conv2D(filters=12, kernel_size=(3, 3), activation=tf.nn.relu),
-    keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    keras.layers.Flatten(),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
-])
-
-# Define how to train the model
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-# Train the digit classification model
-model.fit(train_images, train_labels, epochs=5)
-
-# model summary
-model.summary()
-
-'''
-# Evaluate the model using all images in the test dataset.
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('Test accuracy:', test_acc)
+import cv2
+import time
 
 
-# A helper function that returns 'red'/'black' depending on if its two input
-# parameter matches or not.
-def get_label_color(val1, val2):
-    if val1 == val2:
-        return 'black'
-    else:
-        return 'red'
+def prepare_net():
+    mnist = tf.keras.datasets.mnist
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=5) # model training
+    model.evaluate(x_test,  y_test, verbose=2) # model evaluation
+
+    return model
 
 
-# Predict the labels of digit images in our test dataset.
-predictions = model.predict(test_images)
+def main():
+    mnist_net = prepare_net()
 
-# As the model output 10 float representing the probability of the input image
-# being a digit from 0 to 9, we need to find the largest probability value
-# to find out which digit the model predicts to be most likely in the image.
-prediction_digits = np.argmax(predictions, axis=1)
+    cap = cv2.VideoCapture(0)
 
-# Then plot 100 random test images and their predicted labels.
-# If a prediction result is different from the label provided label in "test"
-# dataset, we will highlight it in red color.
-plt.figure(figsize=(18, 18))
-for i in range(100):
-    ax = plt.subplot(10, 10, i + 1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    image_index = random.randint(0, len(prediction_digits))
-    plt.imshow(test_images[image_index], cmap=plt.cm.gray)
-    ax.xaxis.label.set_color(get_label_color(prediction_digits[image_index], test_labels[image_index]))
-    plt.xlabel('Predicted: %d' % prediction_digits[image_index])
-plt.show()
-'''
+    while True:
+        # Capture frame-by-frame
+        # shape : width, height, channel
+        ret, frame = cap.read()
+        print('frame shpae: {}'.format(frame.shape))
 
-# Evaluation using capture images
-while True:
-    ret, frame = capture.read()
-    cv2.imshow("VideoFrame", frame)
-    predictions = model.predict(frame)
-    prediction_digits = np.argmax(predictions, axis=1)
-    frame_loss, frame_acc = model.evaluate(frame, predictions)
-    print(predictions, prediction_digits, frame_acc, frame_loss)
-    if cv2.waitKey(1) > 0:
-        break
+        ### Our operations on the frame come here
 
-capture.release()
-cv2.destroyAllWindows()
+        # cvtColor : convert color
+        # astype : convert data type from int to float32
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY).astype(np.float32) / 255.0
+        print('gray frame shape: {}'.format(gray.shape))
 
+        # resize : image shape = (28, 28)
+        # 차원을 추가하는 이유는 RGB는 3차원인데, GRAY는 2차원이어서, 1차원 추가해야한다. 그래야 tensorflow에서 사용한다.
+        gray = cv2.resize(gray, (28, 28), interpolation=cv2.INTER_AREA)
+        gray_test = gray[np.newaxis, :, :]
+        print('gray shape: {}'.format(gray.shape))
+
+        # predict class for gray image
+        predict = mnist_net.predict_classes(gray_test)
+        print('predict shape: {}'.format(predict.shape))
+        print('predict: {}'.format(predict))
+
+        # Display the resulting frame
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
